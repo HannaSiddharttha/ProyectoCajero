d6ad1adffc700c116a17c1e33d1c2950793f55fe
Hanna Lizarraga <siddharttha95@gmail.com>
2019-06-03 18:19:23 -0500

    algoritmo voraz implementado

--- cajero.js
+++ cajero.js
@@ -1,25 +1,45 @@
 
 
 var denominaciones = {
-    uno: 25, 
-    dos: 50, 
-    cinco: 30,
-    diez: 40,
-    viente: 6,
-    cincuenta: 10,
-    cien: 25,
-    docientos: 35,
+    quinientos: 500,
+    doscientos: 200,
+    cien: 100,
+    cincuenta: 50,
+    veinte: 20,
+    diez: 10,
+    cinco: 5,
+    dos: 2,
+    uno: 1
+};
+
+var caja = {
     quinientos: 22,
-    mil: 10,
+    doscientos: 30,
+    cien: 25,
+    cincuenta: 10,
+    veinte: 6,
+    diez: 40,
+    cinco: 30,
+    dos: 50,
+    uno: 25
 };
 
 function imprimirEstadoCaja() {
     console.log("\nEstado actual de la caja: \n");
-    console.log("1 peso:  " + denominaciones.uno   + "     2 pesos:  "  + denominaciones.dos);
-    console.log("5 pesos: " + denominaciones.cinco + "     10 pesos: " + denominaciones.diez);
-    console.log("20 pesos:  " + denominaciones.veinte + "  50 pesos:  "  + denominaciones.cincuenta);
-    console.log("100 pesos: " + denominaciones.cien + "    200 pesos: " + denominaciones.docientos);
-    console.log("500 pesos: " + denominaciones.quinientos + "  1000 pesos: " + denominaciones.mil);
+    console.log("1 peso:  " + caja.uno   + "     2 pesos:  "  + caja.dos);
+    console.log("5 pesos: " + caja.cinco + "     10 pesos: " + caja.diez);
+    console.log("20 pesos:  " + caja.veinte + "  50 pesos:  "  + caja.cincuenta);
+    console.log("100 pesos: " + caja.cien + "    200 pesos: " + caja.doscientos);
+    console.log("500 pesos: " + caja.quinientos);
+}
+
+function imprimirRetirado(retirado) {
+    console.log("Retirado: \n");
+    for(var key in retirado) {
+        if(retirado[key] > 0) {
+            console.log(key + ": "+ retirado[key] + "\n");
+        }
+    }
 }
 
 const readline = require('readline').createInterface({
@@ -28,15 +48,53 @@ const readline = require('readline').createInterface({
   })
   
 readline.question(`Cuanto dinero desea retirar?`, (monto) => {
-    console.log("\nSe retiraron 1450 pesos \n");
+    //console.log("\nSe retiraron 1450 pesos \n");
+    var totalEnCaja = 3000;
     imprimirEstadoCaja();
-    denominaciones.docientos = denominaciones.docientos - 2;
-    denominaciones.cincuenta = denominaciones.cincuenta - 1;
-    denominaciones.mil = denominaciones.mil - 1;
+
+    if(monto > totalEnCaja) {
+        console.log("\n Fondos insuficientes \n");
+        readline.close()
+    }
+
+    var retirado = reajustarCaja(monto);
+    imprimirRetirado(retirado)
+
+
     imprimirEstadoCaja();
     readline.close()
 })
 const fs = require('fs');
+
+function reajustarCaja(monto) {
+    var retirado = {
+        quinientos: 0,
+        doscientos: 0,
+        cien: 0,
+        cincuenta: 0,
+        veinte: 0,
+        diez: 0,
+        cinco: 0,
+        dos: 0,
+        uno: 0
+    };
+    var montoActual = monto;
+    for(var key in denominaciones) {
+
+        var acumular = true;
+        while(acumular) {
+            //Si el monto a retirar es mayor a la denominacion del billete y hay billetes disponibles...
+            if(montoActual >= denominaciones[key] && caja[key] > 0) {
+                caja[key] = caja[key] - 1;
+                retirado[key] += 1;
+                montoActual = montoActual - denominaciones[key];
+            } else {
+                acumular = false;
+            }
+        }
+    }
+    return retirado;
+}
 
 function loadFileData(fileName) {
     return JSON.parse(fs.readFileSync(fileName).toString());
@@ -47,11 +105,11 @@ function saveFileData(fileName, data) {
     fs.writeFileSync(fileName, JSON.stringify(data));
 }
 
-const usuario = loadFileData('usuario.dat');
-console.log(usuario);
+//const usuario = loadFileData('usuario.dat');
+//console.log(usuario);
 
-let usuario = usuarios.find(p => p.id == 3);
-console.log(usuario);
-usuario.description = "Hanna";
-saveFileData('otros-usuarios.dat', usuarios);
+//let usuario = usuarios.find(p => p.id == 3);
+//console.log(usuario);
+//usuario.description = "Hanna";
+//saveFileData('otros-usuarios.dat', usuarios);
 //fs.writeFileSync('otros-productos.dat', JSON.stringify(products));
